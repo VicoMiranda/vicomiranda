e695321b4e92de7ad9da92e54c77ded5b4f7d96f
Victor Miranda <vmsoberanis@gmail.com>
2022-06-06 14:56:44 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import React from "react"
+import vico from './vico.jpg'
 
 export default function Home() {
   return (
