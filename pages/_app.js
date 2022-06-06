2e73f3aedf00928b998a2d45089ccbdaff7a2b4d
Victor Miranda <vmsoberanis@gmail.com>
2022-06-06 17:31:52 +1200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<Component {...pageProps} />
+<p>
+    Hello
+</p>
+);
 }
 
 export default Application
